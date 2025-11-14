1052289964193e7e19b3c372c9f5850ba14e7a9c
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-11-15 00:47:15 +0200

    Урок 2: Основи синтаксису Python: змінні, оператори, типи даних.

---
+++ main.py
@@ -0,0 +1,106 @@
+# num_1 = 5
+# print(type(num_1))
+
+# num_2 = 3.14
+# print(type(num_2))
+
+# string = 'hello'
+
+# print(type(string))
+
+# check = True
+# print(type(check))
+
+# list = ['hello', 'my', 'world']
+# print(type(list))
+
+# tpl = (1,2,3)
+# print(type(tpl))
+
+# dct = {'name': 'Jonh', 'age': 23}
+# print(type(dct))
+
+# set_ex = {1,2,3}
+# print(type(set_ex))
+
+# print(type(None))
+
+# class Person:
+#     pass
+
+# a = Person()
+# print(type(a))
+
+
+# lst = [1,2,3,4,5]
+# dct = {'name': 'Tom', 'age': 5}
+# name = 'Tom'
+# tpl = ('n', 'a', 'g')
+
+
+# print(dct['name'] == name and dct['age'] in lst)
+
+
+# num_1 = '1'
+# print(type(num_1))
+# num_1 = int(num_1)
+# print(type(num_1))
+# num_1 = float(num_1)
+# print(num_1)
+# print(type(num_1))
+
+# string = 'hello world'
+# print(len(string))
+# string = string.upper()
+# print(string)
+# string = string.lower()
+# print(string)
+# string = string.capitalize()
+# print(string)
+# string = string.replace('d', 'f')
+# print(string)
+# string = string.split()
+# print(string)
+# # string = string.count('o')
+# # print(string)
+# string = '  '.join(string)
+# print(string)
+# string = string.count('o')
+# print(string)
+# string = 1
+# string = str(string)
+# print(type(string))
+
+# base_list = [1,2,3]
+# print(len(base_list))
+
+# base_list.append(4)
+# print(base_list)
+
+# base_list.extend([5,6,7])
+# print(base_list)
+
+# print(base_list.index(4))
+
+# base_dict = {'name': 'Tom', 'age': 40, 'high': 180}
+# print(base_dict.keys())
+# print(base_dict.values())
+# print(base_dict.items())
+
+# print(base_dict['name'], base_dict.get('animal', 'No'))
+# print(base_dict['animal'])
+
+num_1 = '1'
+string = 'Hello world!'
+num_1 = int(num_1)
+num_1 = len(string)
+base_dict = {'world': string, 'letter': num_1}
+print(base_dict.values())
+print(base_dict['letter'])
+
+list = ['age', 'goat', 'car']
+list.append('gear')
+print(list)
+print(list.index('gear'))
+print(len(list))
+print(len(list) == 4 and base_dict['letter'] != 12)
