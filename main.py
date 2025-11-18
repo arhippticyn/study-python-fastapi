bf11aaf591514731f222ad7c7234f929e18ffa59
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-11-19 00:44:12 +0200

    Введення в ООП. Класи та об'єкти.

--- main.py
+++ main.py
@@ -359,37 +359,314 @@
 
 #4
 
-numbers = [3, 7, 2, 9, 5]
+# numbers = [3, 7, 2, 9, 5]
 
-numbers.append(10)
-numbers.remove(7)
-numbers.sort()
+# numbers.append(10)
+# numbers.remove(7)
+# numbers.sort()
 
-print(numbers)
+# print(numbers)
 
 #5
 
-t = (1, 2, 3, 4, 5)
+# t = (1, 2, 3, 4, 5)
 
-list = list(t)
-list.append(6)
+# list = list(t)
+# list.append(6)
 
-print(tuple(list))
+# print(tuple(list))
 
-#6
+# #6
 
-user = {
-    'name': 'Anna',
-    'age': 21,
-    'city': 'Lviv'
-}
+# user = {
+#     'name': 'Anna',
+#     'age': 21,
+#     'city': 'Lviv'
+# }
 
-user_update = {
-    'is_student': True
-}
+# user_update = {
+#     'is_student': True
+# }
 
-user.update(user_update)
+# user.update(user_update)
 
-print(user)
+# print(user)
 
 
+# Урок 5: Функції та модулі.
+
+# def say_hello(username, age):
+#     print(f'Hello {username} welcome')
+#     print(f'Your age is {age}')
+#     print('-----------------')
+    
+# def print_numbers(start, stop):
+#     for num in range(start, stop):
+#         print(f'Current num is {num}')
+        
+#     print('-----------------')
+        
+# user_data = {'Dima':25, 'Sarah': 34, 'Tom': 11}
+# list_of_ranges = [(1,10), (2,9), (0,100)] 
+
+
+# # for name, age in user_data.items():
+# #     say_hello(name, age)
+    
+# for start_pos, stop_pos in list_of_ranges:
+#   print_numbers(start_pos, stop_pos)
+
+
+# def check_connection(username,count_tries, priority):
+#     if priority >= 10:
+#         finish = 5
+#         for attemt in range(1, count_tries + 1):
+#             if attemt == finish:
+#                 print('Connect was successfully')
+#                 break
+#             print(f'Attemp: {attemt} to connect to {username}')
+            
+#     elif priority >= 5 and priority < 10:
+#         finish = 3
+#         for attemt in range(1,6):
+#             if attemt == finish:
+#               print('Connect was successfully')
+#             print(f'Attemp: {attemt} to connect to username')
+            
+#     else: 
+#         print('Yout username has so low priority')
+        
+# check_connection(count_tries=10, username='Oleg', priority=100)
+
+# import turtle
+
+# def drawSquare(size, color):
+#     turtle.speed(1)
+#     turtle.color(color)
+#     turtle.begin_fill()
+#     def move(len):
+#         turtle.forward(len)
+#         turtle.left(90)
+        
+#     for _ in range(1,5):
+#         move(size)
+        
+#     turtle.end_fill()
+
+# drawSquare(100, 'red')
+# turtle.goto(100,100)
+# drawSquare(200, 'blue')
+
+# Урок 5: Функції та модулі. (практика)
+
+# Створи функцію filter_even(numbers), яка приймає список чисел і повертає новий список, що містить тільки парні числа.
+
+# def filter_even(numbers):
+#     num_par = []
+    
+#     for num in numbers:
+#         if num % 2 == 0:
+#             num_par.append(num)    
+#             print(num_par)
+            
+# filter_even([1, 2, 3, 4, 5, 6])  # → [2, 4, 6]
+
+# Напиши функцію count_vowels(s), яка повертає кількість голосних (a, e, i, o, u) у рядку.
+
+# def count_vowels(s):
+#     gol = 'aeiouAEIOU'
+    
+#     count = 0
+#     for let in s:
+#         if let in gol:
+#             count += 1
+#     return count        
+            
+# print(count_vowels("hello world"))
+
+
+# Створи функцію buy(product_name, amount), яка:перевіряє, чи є такий товар,перевіряє, чи вистачає кількості,
+# якщо так — зменшує кількість, якщо ні — виводить відповідне повідомлення
+
+# products = {
+#     "apple": 10,
+#     "banana": 5,
+#     "milk": 2
+# }
+
+# def buy(product_name, amount): 
+#     if product_name not in products:
+#         return 'Products not found'
+        
+#     current_amount = products[product_name]
+#     if current_amount < amount:
+#         return f"Помилка: На складі лише {current_amount} одиниць товару '{product_name}', а ви просите {amount}."
+    
+#     products[product_name] -= amount
+    
+#     return f'Yoy buy {product_name} is prev amount {amount}, now your amount {products[product_name]}'
+   
+    
+# print(buy("apple", 3))  
+# print(buy("orange", 1))
+# print(buy("milk", 5))
+
+
+# Урок 6: Введення в ООП. Класи та об'єкти.
+
+
+# class Person:
+#     """Class for creation persons"""
+#     name = 'Tom'
+#     age = 18
+#     high = 180
+    
+# print(Person.name)
+# print(Person.age)
+# Person.age = 50
+# print(Person.name)
+# print(Person.age)
+
+# print(Person.__dict__)
+
+# person1 = Person()
+# person1.is_animal = False
+# print(person1.__dict__)
+# print(Person.__dict__)
+
+# person2 = Person()
+# print(person2.__dict__)
+# print(person1.where_is)
+# print(getattr(person1, 'name'))
+# print(getattr(person1, 'where_is', False))
+
+# person1.age = 59
+# person1.color = 'black'
+# print(person1.__dict__)
+
+# setattr(person1, 'high', 100)
+# print(person1.high)
+# print(person1.__dict__)
+
+# print(hasattr(person1, 'name'))
+# print(hasattr(person1, 'where_is'))
+
+# del Person.high
+# print(Person.__dict__)
+# print(hasattr(Person, 'high'))
+
+# delattr(Person, 'age')
+# print(hasattr(Person, 'age'))
+
+# getattr, setattr, hasattr, delattr
+
+# print(Person.__doc__)
+
+# class Person:
+#     '''Class for creation persons'''
+    
+#     def __init__(self, name, age):
+#         self.name = name 
+#         self.age = age
+        
+#     def print_attrs(self):
+#         print(f'>>> {self} <<<')
+#         print(self.name, self.age)
+        
+# person1 = Person('Tom', 18)
+# print(person1)
+# person1.print_attrs()
+
+# person2 = Person('oleg', 50)
+# print(person2)
+# person2.print_attrs()
+
+# class Point: 
+#     '''Class for create and set coords'''
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#         self.get_attrs()
+#         self.check_coords()
+        
+#     def check_coords(self):
+#         for attr in self.__dict__:
+#             if getattr(self, attr, False) < 0 and not isinstance(self.__dict__[attr],str):
+#                 print('coord cant be less than 0')
+#                 setattr(self, attr, 0)
+#             elif getattr(self, attr, False) > 100 and not isinstance(self.__dict__[attr],str):
+#                 print('coord cant be great than 100')
+#                 setattr(self, attr, 100)
+#             print(self.__dict__)
+            
+#     def get_attrs(self):
+#         print(self.__dict__)
+        
+#     def set_attrs(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#         self.check_coords()
+        
+    
+        
+# coord_1 = Point(-1,101,50)
+# coord_1.get_attrs()
+# coord_1.check_coords()
+# print('-------')
+# coord_1.set_attrs(1000, 1000, -5)
+# coord_1.get_attrs()
+# coord_1.check_coords()
+
+# Урок 6: Введення в ООП. Класи та об'єкти. (практика)
+
+# 1 Створи клас Car, який має: Атрибути:brand (марка),year (рік випуску) Метод: info() — повертає рядок: "Car: <brand>, year: <year>"
+
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+        
+    def info(self):
+        brand = self.brand
+        year = self.year
+        print(f'Car: {brand}, year: {year}')
+        
+car1 = Car('BMW', 2018)
+car1.info()
+
+
+# Створи клас User, який має:
+# Атрибути:name,balance (початково передається в конструкторі)Методи:add_balance(amount) — додає гроші, info() — повертає:"User: Alex, balance: 200"
+
+class User:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        
+    def add_balance(self,amount):
+        self.balance += amount
+        
+    def info(self):
+        return f'User: {self.name}, balance: {self.balance}'
+        
+user1 = User('Alex',100)
+user1.add_balance(50)
+print(user1.info())
+
+# Створи клас Product, який має: Атрибути:name, price Метод:discount(percent) — зменшує ціну на відсоток
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+    def discount(self, persent):
+        
+        self.price = round(self.price * (1 - persent / 100), 2)
+    
+product_1 = Product('Laptop', 1000)
+print(product_1.__dict__)
+product_1.discount(10)
+print(product_1.price)
